f8e7b740b7a8db67219f5f02ed5b4d52e8d395e2
Jihoo Kim <jihoo94@gmail.com>
2024-10-01 22:18:41 +0900

    fix: create d.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,18 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'react-select-items',
-      fileName: (format) => `index.${format}.ts`,
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
